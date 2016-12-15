f16b5435a7905b3de37ba641ad91c545ae0dbf5b
lvmingyin <lvmy@zhinanmao.com>
2016-12-15 09:32:07 +0800

    add

--- index.js
+++ index.js
@@ -2,18 +2,22 @@
  * Created by MingYin Lv on 2016/12/14.
  */
 
+
 function Last() {
-    var lastPromise = null;
-    return (promise) => {
-        lastPromise = promise;
-        return new Promise((resolve) => {
-            promise.then((data) => {
-                if (lastPromise === promise) {
-                    resolve(data);
-                }
-            });
-        });
-    }
+  var lastPromise = null;
+  /**
+   * @return Promise
+   */
+  return (promise) => {
+    lastPromise = promise;
+    return new Promise((resolve) => {
+      promise.then((data) => {
+        if (lastPromise === promise) {
+          resolve(data);
+        }
+      });
+    });
+  }
 }
 
 module.exports = Last;
